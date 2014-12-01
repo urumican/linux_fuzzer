709fb7560b5ff03af7962635d7ae77298bbd59b9
Xin Li <urumican@users.noreply.github.com>
2014-12-01 15:57:26 -0800

    Create dir_generator.c

---
+++ dir_generator.c
@@ -0,0 +1,56 @@
+#include <dirent.h> 
+#include <stdio.h>
+#include <sys/stat.h>
+#include "my_types.h"
+#include <fcntl.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int determine_dir(char direct[])
+{
+	struct stat status;
+
+	stat(direct, &status);
+
+	if (status.st_mode & S_IFDIR) // is a directory file.
+		return 0;
+	if (status.st_mode & S_IFREG) // is a regular file.
+		return 1;
+}
+
+
+void dir_generator(char *direct, int *index)
+{
+	int regular = 0;
+	int count, i;
+	char *abs_dir;
+	DIR *d;
+	struct dirent *dir;
+
+	d = opendir(direct);
+
+	if (d != NULL)
+	{
+		while ((dir = readdir(d)) != NULL)
+		{
+			if (strcmp(dir->d_name, ".") != 0 && strcmp(dir->d_name, "..") != 0)
+			{	
+				for(i = 0; dir->d_name[i] != '\0'; i++)
+					count++;
+				abs_dir = (char*)malloc((count + 2) * sizeof(char));
+				strcpy(abs_dir, direct);
+				strcat(abs_dir, "/");
+				strcat(abs_dir, dir->d_name);
+
+				pool->dirs[*index] = open(abs_dir, O_RDONLY);
+				(*index)++;
+
+				regular = determine_dir(abs_dir);
+
+				if (regular == 0)
+					fd_generator(abs_dir, index);
+			}
+		}
+	}
+}
